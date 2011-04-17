ca00251393a72d9761c09feb77b4a559424df73d
Martin Mädler <martin@codecoach.de>
2011-04-17 14:11:17 +0200

    Initial commit!

---
+++ YAID3/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YAID3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            // This can't be done any earlier than this:
+            GlassHelper.ExtendGlassFrame(this, new Thickness(-1));
+        }
+
+		private void ctlFiles_DragOver(object sender, DragEventArgs e)
+		{
+			if (e.Data.GetDataPresent(DataFormats.FileDrop, false))
+			{
+				e.Effects = DragDropEffects.Copy;
+			}
+			else
+			{
+				e.Effects = DragDropEffects.None;
+			}
+
+			e.Handled = true;
+		}
+
+		private void ctlFiles_Drop(object sender, DragEventArgs e)
+		{
+			string[] paths = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+			MessageBox.Show(string.Format("You dropped {0} items!", paths.Length));
+		}
+    }
+}
